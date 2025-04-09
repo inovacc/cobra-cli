97cc0b0522b50d8f3661d4782c94a4e9c03df9f8
dyam <dyam.marcano@gmail.com>
2025-04-08 23:29:53 -0300

    feat: refactor add command and project structure for improved command generation

--- cmd/add.go
+++ cmd/add.go
@@ -13,67 +13,66 @@
 
 package cmd
 
-//import (
-//	"bytes"
-//	"fmt"
-//	"github.com/inovacc/cobra-cli/internal/project"
-//	"github.com/spf13/afero"
-//	"github.com/spf13/cobra"
-//	"os"
-//)
-//
-//func init() {
-//	addCmd.SetOut(new(bytes.Buffer))
-//	addCmd.SetErr(new(bytes.Buffer))
-//}
-//
-//var (
-//	packageName string
-//	parentName  string
-//
-//	addCmd = &cobra.Command{
-//		Use:     "add [command name]",
-//		Aliases: []string{"command"},
-//		Short:   "Add a command to a Cobra Application",
-//		Long: `Add (cobra-cli add) will create a new command, with a license and
-//the appropriate structure for a Cobra-based CLI application,
-//and register it to its parent (default rootCmd).
-//
-//If you want your command to be public, pass in the command name
-//with an initial uppercase letter.
-//
-//Example: cobra-cli add server -> resulting in a new cmd/server.go`,
-//		ValidArgsFunction: func(cmd *cobra.Command, args []string, toComplete string) ([]string, cobra.ShellCompDirective) {
-//			var comps []string
-//			if len(args) == 0 {
-//				comps = cobra.AppendActiveHelp(comps, "Please specify the name for the new command")
-//			} else if len(args) == 1 {
-//				comps = cobra.AppendActiveHelp(comps, "This command does not take any more arguments (but may accept flags)")
-//			} else {
-//				comps = cobra.AppendActiveHelp(comps, "ERROR: Too many arguments specified")
-//			}
-//			return comps, cobra.ShellCompDirectiveNoFileComp
-//		},
-//		Run: func(cmd *cobra.Command, args []string) {
-//			if len(args) < 1 {
-//				cobra.CheckErr(fmt.Errorf("add needs a name for the command"))
-//			}
-//
-//			wd, err := os.Getwd()
-//			cobra.CheckErr(err)
-//
-//			commandName := project.ValidateCmdName(args[0])
-//			newProject := project.NewProject(afero.NewOsFs(), wd, packageName, commandName)
-//			command := project.NewCommand(commandName, parentName, newProject)
-//
-//			cobra.CheckErr(command.Create())
-//			fmt.Printf("%s created at %s\n", command.CmdName, command.AbsolutePath)
-//		},
-//	}
-//)
-//
-//func init() {
-//	addCmd.Flags().StringVarP(&packageName, "package", "t", "", "target package name (e.g. github.com/spf13/hugo)")
-//	addCmd.Flags().StringVarP(&parentName, "parent", "p", "rootCmd", "variable name of parent command for this command")
-//	cobra.CheckErr(addCmd.Flags().MarkDeprecated("package", "this operation has been removed."))
-//}
+import (
+	"bytes"
+	"fmt"
+	"github.com/inovacc/cobra-cli/internal/project"
+	"github.com/spf13/afero"
+	"github.com/spf13/cobra"
+)
+
+func init() {
+	addCmd.SetOut(new(bytes.Buffer))
+	addCmd.SetErr(new(bytes.Buffer))
+}
+
+var (
+	packageName string
+	parentName  string
+
+	addCmd = &cobra.Command{
+		Use:     "add [command name]",
+		Aliases: []string{"command"},
+		Short:   "Add a command to a Cobra Application",
+		Long: `Add (cobra-cli add) will create a new command, with a license and
+the appropriate structure for a Cobra-based CLI application,
+and register it to its parent (default rootCmd).
+
+If you want your command to be public, pass in the command name
+with an initial uppercase letter.
+
+Example: cobra-cli add server -> resulting in a new cmd/server.go`,
+		ValidArgsFunction: func(cmd *cobra.Command, args []string, toComplete string) ([]string, cobra.ShellCompDirective) {
+			var comps []string
+			if len(args) == 0 {
+				comps = cobra.AppendActiveHelp(comps, "Please specify the name for the new command")
+			} else if len(args) == 1 {
+				comps = cobra.AppendActiveHelp(comps, "This command does not take any more arguments (but may accept flags)")
+			} else {
+				comps = cobra.AppendActiveHelp(comps, "ERROR: Too many arguments specified")
+			}
+			return comps, cobra.ShellCompDirectiveNoFileComp
+		},
+		Run: func(cmd *cobra.Command, args []string) {
+			if len(args) < 1 {
+				cobra.CheckErr(fmt.Errorf("add needs a name for the command"))
+			}
+
+			afs := afero.NewOsFs()
+			newProject, err := project.NewProject(args)
+			cobra.CheckErr(err)
+
+			projectGenerator, err := project.NewProjectGenerator(afs, newProject)
+			cobra.CheckErr(err)
+
+			cobra.CheckErr(projectGenerator.AddCommandProject())
+			fmt.Printf("%s created at %s\n", projectGenerator.CmdName(), projectGenerator.GetProjectPath())
+		},
+	}
+)
+
+func init() {
+	addCmd.Flags().StringVarP(&packageName, "package", "t", "", "target package name (e.g. github.com/spf13/hugo)")
+	addCmd.Flags().StringVarP(&parentName, "parent", "p", "rootCmd", "variable name of parent command for this command")
+	cobra.CheckErr(addCmd.Flags().MarkDeprecated("package", "this operation has been removed."))
+}
--- internal/project/project.go
+++ internal/project/project.go
@@ -2,23 +2,138 @@ package project
 
 import (
 	"bufio"
+	"bytes"
 	"embed"
+	"encoding/json"
 	"errors"
 	"fmt"
 	"github.com/spf13/afero"
 	"github.com/spf13/cobra"
 	"github.com/spf13/viper"
+	"log"
 	"os"
+	"os/exec"
 	"path"
 	"path/filepath"
 	"regexp"
 	"strings"
 	"text/template"
+	"time"
 	"unicode"
 )
 
 //go:embed tpl/*.tmpl
 var templates embed.FS
+
+var srcPaths []string
+
+func init() {
+	// Initialize srcPaths.
+	envGoPath := os.Getenv("GOPATH")
+	goPaths := filepath.SplitList(envGoPath)
+	if len(goPaths) == 0 {
+		// Adapted from https://github.com/Masterminds/glide/pull/798/files.
+		// As of Go 1.8 the GOPATH is no longer required to be set. Instead, there
+		// is a default value. If there is no GOPATH check for the default value.
+		// Note, checking the GOPATH first to avoid invoking the go toolchain if
+		// possible.
+
+		goExecutable := os.Getenv("COBRA_GO_EXECUTABLE")
+		if len(goExecutable) <= 0 {
+			goExecutable = "go"
+		}
+
+		out, err := exec.Command(goExecutable, "env", "GOPATH").Output()
+		cobra.CheckErr(err)
+
+		toolchainGoPath := strings.TrimSpace(string(out))
+		goPaths = filepath.SplitList(toolchainGoPath)
+		if len(goPaths) == 0 {
+			cobra.CheckErr("$GOPATH is not set")
+		}
+	}
+	srcPaths = make([]string, 0, len(goPaths))
+	for _, goPath := range goPaths {
+		srcPaths = append(srcPaths, filepath.Join(goPath, "src"))
+	}
+}
+
+// ensureLF converts any \r\n to \n
+func ensureLF(content []byte) []byte {
+	return bytes.ReplaceAll(content, []byte("\r\n"), []byte("\n"))
+}
+
+func CompareContent(contentA, contentB []byte) error {
+	if !bytes.Equal(ensureLF(contentA), ensureLF(contentB)) {
+		output := new(bytes.Buffer)
+		_, _ = fmt.Fprintf(output, "Contents are not equal!\n\n")
+
+		diffPath, err := exec.LookPath("diff")
+		if err != nil {
+			// Don't execute diff if it can't be found.
+			return nil
+		}
+		diffCmd := exec.Command(diffPath, "-u", "--strip-trailing-cr")
+		diffCmd.Stdin = bytes.NewReader(contentA)
+		diffCmd.Stdout = output
+		diffCmd.Stderr = output
+
+		if err := diffCmd.Run(); err != nil {
+			_, _ = fmt.Fprintf(output, "\n%s", err.Error())
+		}
+		return errors.New(output.String())
+	}
+	return nil
+}
+
+func getModImportPath() string {
+	mod, cd := parseModInfo()
+	return path.Join(mod.Path, fileToURL(strings.TrimPrefix(cd.Dir, mod.Dir)))
+}
+
+func fileToURL(in string) string {
+	i := strings.Split(in, string(filepath.Separator))
+	return path.Join(i...)
+}
+
+type Mod struct {
+	Path, Dir, GoMod string
+}
+
+type CurDir struct {
+	Dir string
+}
+
+func parseModInfo() (Mod, CurDir) {
+	var (
+		mod Mod
+		dir CurDir
+	)
+
+	m := modInfoJSON("-m")
+	cobra.CheckErr(json.Unmarshal(m, &mod))
+
+	// Unsure why, but if no module is present Path is set to this string.
+	if mod.Path == "command-line-arguments" {
+		cobra.CheckErr("Please run `go mod init <MODNAME>` before `cobra-cli init`")
+	}
+
+	e := modInfoJSON("-e")
+	cobra.CheckErr(json.Unmarshal(e, &dir))
+
+	return mod, dir
+}
+
+func GoGet(mod string) error {
+	return exec.Command("go", "get", mod).Run()
+}
+
+func modInfoJSON(args ...string) []byte {
+	cmdArgs := append([]string{"list", "-json"}, args...)
+	out, err := exec.Command("go", cmdArgs...).Output()
+	cobra.CheckErr(err)
+	return out
+}
 
 type Command struct {
 	CmdName   string
@@ -31,6 +146,7 @@ type Project struct {
 	PkgName      string
 	AbsolutePath string
 	AppName      string
+	CmdName      string
 	Legal        *License
 }
 
@@ -74,6 +190,8 @@ type Generator struct {
 }
 
 func NewProjectGenerator(fs afero.Fs, project *Project) (*Generator, error) {
+	project.CmdName = validateCmdName(project.Args[0])
+
 	return &Generator{
 		afs:       fs,
 		templates: templates,
@@ -101,6 +219,10 @@ type Content struct {
 
 func (g *Generator) GetProjectPath() string {
 	return g.project.AbsolutePath
+}
+
+func (g *Generator) CmdName() string {
+	return g.project.CmdName
 }
 
 // CreateProject sets up the project structure and files.
@@ -133,6 +255,10 @@ func (g *Generator) CreateProject() error {
 
 // AddCommandProject sets up the project structure and files for a new command.
 func (g *Generator) AddCommandProject() error {
+	if !g.parentProject() {
+		return errors.New("no parent project")
+	}
+
 	if !g.findLicense() {
 		return errors.New("no legal project")
 	}
@@ -239,15 +365,17 @@ func (g *Generator) getFileContentSub() (Content, error) {
 		return Content{Dirty: true}, err
 	}
 
+	command := Command{
+		CmdParent: "rootCmd",
+		CmdName:   g.project.CmdName,
+		Project:   g.project,
+	}
+
 	return Content{
 		Name:            "add_command", // from input command
 		FilePath:        subPath,
 		TemplateContent: string(data),
-		Data: Command{
-			CmdParent: "rootCmd",
-			CmdName:   g.validateCmdName(g.project.Args[0]),
-			Project:   g.project,
-		},
+		Data:            command,
 	}, nil
 }
 
@@ -338,7 +466,34 @@ func (g *Generator) findLicense() bool {
 	return true
 }
 
-func (g *Generator) validateCmdName(source string) string {
+func (g *Generator) extractAuthorFromFile(filePath string) (string, error) {
+	file, err := g.afs.Open(filePath)
+	if err != nil {
+		return "", err
+	}
+	defer func(file afero.File) {
+		if err := file.Close(); err != nil {
+			log.Println(err)
+		}
+	}(file)
+
+	re := regexp.MustCompile(`^Copyright © \d{4}\s+`)
+
+	scanner := bufio.NewScanner(file)
+	for scanner.Scan() {
+		line := scanner.Text()
+		if re.MatchString(line) {
+			return re.ReplaceAllString(line, ""), nil
+		}
+	}
+	return "", fmt.Errorf("no matching copyright line found")
+}
+
+func (g *Generator) parentProject() bool {
+
+}
+
+func validateCmdName(source string) string {
 	i := 0
 	l := len(source)
 	// The output is initialized on demand, then first dash or underscore
@@ -387,21 +542,102 @@ func (g *Generator) validateCmdName(source string) string {
 	return output
 }
 
-func (g *Generator) extractAuthorFromFile(filePath string) (string, error) {
-	file, err := g.afs.Open(filePath)
-	if err != nil {
-		return "", err
+type License struct {
+	code            string   // The code name of the license
+	Name            string   // The type of license in use
+	PossibleMatches []string // Similar names to guess
+	Header          string   // License header for source files
+	Body            string   // License body
+	Copyright       string   // Copyright line
+}
+
+func newLicense(templates embed.FS) (*License, error) {
+	year := viper.GetString("year")
+	if year == "" {
+		year = time.Now().Format("2006")
 	}
-	defer file.Close()
 
-	re := regexp.MustCompile(`^Copyright © \d{4}\s+`)
+	licenseDefinitions := map[string]*License{
+		"apache2": {
+			code:            "apache_2",
+			Name:            "Apache 2.0",
+			PossibleMatches: []string{"Apache-2.0", "apache", "apache20", "apache 2.0", "apache2.0", "apache-2.0"},
+		},
+		"mit": {
+			code:            "mit",
+			Name:            "MIT License",
+			PossibleMatches: []string{"MIT", "mit"},
+		},
+		"bsd-3": {
+			code:            "bsd_clause_3",
+			Name:            "NewBSD",
+			PossibleMatches: []string{"BSD-3-Clause", "bsd", "newbsd", "3 clause bsd", "3-clause bsd"},
+		},
+		"bsd-2": {
+			code:            "bsd_clause_2",
+			Name:            "Simplified BSD License",
+			PossibleMatches: []string{"BSD-2-Clause", "freebsd", "simpbsd", "simple bsd", "2-clause bsd", "2 clause bsd", "simplified bsd license"},
+		},
+		"gpl-2": {
+			code:            "gpl_2",
+			Name:            "GNU General Public License 2.0",
+			PossibleMatches: []string{"GPL-2.0", "gpl2", "gnu gpl2", "gplv2"},
+		},
+		"gpl-3": {
+			code:            "gpl_3",
+			Name:            "GNU General Public License 3.0",
+			PossibleMatches: []string{"GPL-3.0", "gpl3", "gplv3", "gpl", "gnu gpl3", "gnu gpl"},
+		},
+		"lgpl": {
+			code:            "lgpl",
+			Name:            "GNU Lesser General Public License",
+			PossibleMatches: []string{"LGPL-3.0", "lgpl", "lesser gpl", "gnu lgpl"},
+		},
+		"agpl": {
+			code:            "agpl",
+			Name:            "GNU Affero General Public License",
+			PossibleMatches: []string{"AGPL-3.0", "agpl", "affero gpl", "gnu agpl"},
+		},
+	}
 
-	scanner := bufio.NewScanner(file)
-	for scanner.Scan() {
-		line := scanner.Text()
-		if re.MatchString(line) {
-			return re.ReplaceAllString(line, ""), nil
+	def, ok := licenseDefinitions[viper.GetString("license")]
+	if !ok {
+		def = &License{
+			code:            "none",
+			Name:            "None",
+			PossibleMatches: []string{"none", "false"},
 		}
 	}
-	return "", fmt.Errorf("no matching copyright line found")
+
+	def.Copyright = fmt.Sprintf("Copyright © %s %s", year, viper.GetString("author"))
+
+	if def.code != "none" {
+		if err := def.getLicenseHeader(templates); err != nil {
+			return nil, err
+		}
+
+		if err := def.getLicenseBody(templates); err != nil {
+			return nil, err
+		}
+	}
+
+	return def, nil
+}
+
+func (l *License) getLicenseHeader(templates embed.FS) error {
+	data, err := templates.ReadFile(fmt.Sprintf("tpl/header_%s.tmpl", l.code))
+	if err != nil {
+		return err
+	}
+	l.Header = string(data)
+	return nil
+}
+
+func (l *License) getLicenseBody(templates embed.FS) error {
+	data, err := templates.ReadFile(fmt.Sprintf("tpl/license_%s.tmpl", l.code))
+	if err != nil {
+		return err
+	}
+	l.Body = string(data)
+	return nil
 }
